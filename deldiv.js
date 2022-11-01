a91037b30281155eb864f0b12f3acaad76927fb1
Stephen McCulloch <stephen.mcculloch@gmail.com>
2022-11-01 13:47:48 +0100

    Update deldiv.js

--- deldiv.js
+++ deldiv.js
@@ -1,7 +1,6 @@
 (function () {
   javascript: (function () {
-    alert('go!');
-    var divElement = document.querySelector('body div:first-child');
-    divElement.style.backgroundColor = 'red';
+    var divElement = document.querySelector('body > div:first-of-type');
+    divElement.remove();
   })();
 })();
